502dd97a705dea48d70ceebf86ba8fdd6bd234d4
Henri Menke <henri@henrimenke.de>
2023-12-12 14:39:34 +0100

    req2flatpak: init at 0.2

--- default.nix
+++ default.nix
@@ -7,4 +7,5 @@ lib.recurseIntoAttrs {
   local-bin = import ./local-bin { inherit pkgs; };
   klatexformula = libsForQt5.callPackage ./klatexformula {};
   prometheus-slurm-exporter = callPackage ./prometheus-slurm-exporter {};
+  req2flatpak = callPackage ./req2flatpak {};
 }
---
+++ req2flatpak/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, fetchFromGitHub
+, python3
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "req2flatpak";
+  version = "0.2";
+  format = "pyproject";
+
+  src = fetchFromGitHub {
+    owner = "johannesjh";
+    repo = "req2flatpak";
+    rev = "v${version}";
+    sha256 = "sha256-Q4lqlLJ59DiXy6fClU2dcwR/5eWM9s0QpYTDWP1UQEI=";
+  };
+
+  nativeBuildInputs = with python3.pkgs; [ poetry-core ];
+  propagatedBuildInputs = with python3.pkgs; [ setuptools ];
+
+  meta = {
+    description = "Convert Python package requirements to Flatpak build manifests";
+    homepage = "https://johannesjh.github.io/req2flatpak/";
+    license = lib.licenses.mit;
+    mainProgram = "req2flatpak";
+    maintainers = with lib.maintainers; [ hmenke ];
+  };
+}
