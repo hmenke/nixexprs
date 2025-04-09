0704bc3b1a90525ca67451107bd8f322edb229fb
Henri Menke <henri@henrimenke.de>
2025-04-09 10:57:11 +0200

    req2flatpak: 0.2 -> 0.3.1

--- req2flatpak/default.nix
+++ req2flatpak/default.nix
@@ -5,15 +5,20 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "req2flatpak";
-  version = "0.2";
+  version = "0.3.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "johannesjh";
     repo = "req2flatpak";
     rev = "v${version}";
-    hash = "sha256-Q4lqlLJ59DiXy6fClU2dcwR/5eWM9s0QpYTDWP1UQEI=";
+    hash = "sha256-gj0SP4SQpkaT1dCov1l4qVWhy5J3Q+s9q3Q7TN83H/Q=";
   };
+
+  postPatch = ''
+    substituteInPlace ./pyproject.toml \
+      --replace-fail 'packaging = { version = "^21.3" }' 'packaging = { version = ">=21.3" }'
+  '';
 
   nativeBuildInputs = with python3.pkgs; [ poetry-core ];
   propagatedBuildInputs = with python3.pkgs; [ setuptools pyyaml ];
