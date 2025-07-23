22d03ef0879ccd72af8d136eb5d61e919cf271a9
Henri Menke <henri@henrimenke.de>
2025-07-23 09:13:12 +0200

    local-bin: revert Go version bump for gh

--- local-bin/default.nix
+++ local-bin/default.nix
@@ -120,6 +120,19 @@ let
       doCheck = false;
     });
 
+    ghStatic = pkgs.gh.overrideAttrs (oa: {
+      env.CGO_ENABLED = 0;
+      ldflags = (oa.ldflags or []) ++ [ "-s" "-w" "-extldflags '-static'" ];
+      patches = (oa.patches or []) ++ lib.optionals (lib.strings.versionAtLeast oa.version "2.76.0") [
+        (fetchpatch {
+          name = "Bump-Go-to-1.24.5.patch";
+          url = "https://github.com/cli/cli/commit/228fbe31dc231d089bc3383bf80bb7904a11954f.patch?full_index=1";
+          hash = "sha256-EJDDMO12jee0lfgIXwExvbV4SCZwZIkR5SK+grlKHIw=";
+          revert = true;
+        })
+      ];
+    });
+
   in {
     age = "${goLinkStatic pkgs.age {}}/bin/age";
     age-keygen = "${goLinkStatic pkgs.age {}}/bin/age-keygen";
@@ -141,7 +154,7 @@ let
     fq = "${goLinkStatic pkgs.fq {}}/bin/fq";
     freeze = "${goLinkStatic pkgs.charm-freeze {}}/bin/freeze";
     fzf = "${goLinkStatic pkgs.fzf {}}/bin/fzf";
-    gh = "${goLinkStatic pkgs.gh {}}/bin/gh";
+    gh = "${ghStatic}/bin/gh";
     glab = "${goLinkStatic pkgs.glab {}}/bin/.glab-wrapped";
     gocryptfs = "${goLinkStatic pkgs.gocryptfs { tags = [ "without_openssl" ]; }}/bin/.gocryptfs-wrapped";
     gocryptfs-xray = "${goLinkStatic pkgs.gocryptfs { tags = [ "without_openssl" ]; }}/bin/gocryptfs-xray";
