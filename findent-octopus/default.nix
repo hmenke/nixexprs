8a4eeaeca6b3daac43f4e43c7b83e7639b0fc3e8
Henri Menke <henri@henrimenke.de>
2026-02-25 09:38:06 +0100

    findent-octopus: 1.0-unstable-2025-06-29 -> 1.0-unstable-2026-02-20
    Diff: https://gitlab.com/octopus-code/findent-octopus/-/compare/555c51f2f803ed756fd36e98552a1417e4d599cf...5432c71402157515564b91507616780fb933f3ae

--- findent-octopus/default.nix
+++ findent-octopus/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (final: {
   pname = "findent-octopus";
-  version = "1.0-unstable-2025-06-29";
+  version = "1.0-unstable-2026-02-20";
 
   src = fetchFromGitLab {
     owner = "octopus-code";
     repo = "findent-octopus";
-    rev = "555c51f2f803ed756fd36e98552a1417e4d599cf";
-    hash = "sha256-Sz73cnhYuSfXBQ7UdRsfPJfctDFN64bAqHtxnboNK4Q=";
+    rev = "5432c71402157515564b91507616780fb933f3ae";
+    hash = "sha256-6EZHA6UNFRFsq2avE9Al+PHdu9HfONcBNKLiCGN0le0=";
   };
 
   nativeBuildInputs = [ bison flex ];
