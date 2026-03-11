3ab2f4ae1eedc39401ea3e6a5abfade1328a7742
Henri Menke <henri@henrimenke.de>
2026-03-11 13:21:58 +0100

    local-bin: override btdu with patched version

--- local-bin/btdu-static.nix
+++ local-bin/btdu-static.nix
@@ -3,7 +3,10 @@
 let
   pname = "btdu";
   version = "0.7.2";
-  src = fetchTarball "https://github.com/CyberShadow/${pname}/archive/refs/tags/v${version}.tar.gz";
+  src =
+    fetchTarball
+      #"https://github.com/CyberShadow/${pname}/archive/refs/tags/v${version}.tar.gz";
+      "https://github.com/hmenke/btdu/archive/1afe078933a19962dc5da8e7f56541feb8d77997.tar.gz";
   flake = import "${src}/flake.nix";
   self = null;
   nixpkgs = {
